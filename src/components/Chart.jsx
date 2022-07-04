df496074b2de680ca8c4bfbfaae0f0f97ad9c8e3
Luis Medina <lamc3005@gmail.com>
2022-07-03 21:20:36 -0400

    Change size bar and aplling effect hover on bar

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import data from "../data.json";
 
-
 const Chart = () => {
   const values = data.map((item) => Object.values(item));
-  const maxValue = Math.max(...values.map((item) => item[1]));
-  const percentaje = 100 / maxValue;
-
+  const arrayNumber = values.map((item) => item[1])
+  const maxValue = Math.max(...arrayNumber);
+  const sumTotal = arrayNumber.map(c => parseFloat(c)).reduce((a, b) => a + b, 0);
+  const percentaje = (100 / sumTotal) * 3
+  
   return (
     <div className="chart">
       {values.map((item, index) => {
@@ -14,9 +15,9 @@ const Chart = () => {
         const conditionalClass = maxValue === item[1] ? "bar active" : "bar";
         return (
           <div key={index} className="bar-container">
-            <div className="price">{`$${item[1]}`}</div>
-            <div className={conditionalClass} style={style}></div>
             <p className="bar-text">{item[0]}</p>
+            <div className={conditionalClass} style={style}></div>
+            <div className="price">{`$${item[1]}`}</div>
           </div>
         );
       })}
